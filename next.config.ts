b0b08195aea5c89b2b3ec2c4ba74b1f34371bd58
thebooknest-admin <hello@the-book-nest.com>
2026-02-04 11:05:16 -0500

    Remove PWA for now

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,7 @@
 import type { NextConfig } from "next";
-const withPWA = require('next-pwa')({
-  dest: 'public',
-  register: true,
-  skipWaiting: true,
-})
 
 const nextConfig: NextConfig = {
-  webpack: (config) => {
-    return config;
-  },
+  /* config options here */
 };
 
-export default withPWA(nextConfig);
+export default nextConfig;
